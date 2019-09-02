dac1e23aff1f94cae1471eb33c742f2bb3b9da36
Vinicius Moreira <vinicius.moreira@mss.modulo.com.br>
2019-09-02 14:32:57 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -42,6 +42,8 @@ namespace xadrez_console {
                     }
                     
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
                 
             }
             catch(TabuleiroException e)
